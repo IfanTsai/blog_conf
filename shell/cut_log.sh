12dc0566b22cbe09a7e9a7638c19b2bb2d146cd0
IfanTsai <i@caiyifan.cn>
2021-05-10 22:22:11 +0800

    add shell/cut_log.sh

---
+++ shell/cut_log.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# crontab: 0 0 * * * /bin/bash /usr/local/openresty/nginx/conf/shell/cut_log.sh >/dev/null 2>&1
+
+logs_path=/usr/local/openresty/nginx/logs/
+pid_path=/usr/local/openresty/nginx/logs/nginx.pid
+
+servers=('ngx' 'hexo' 'api' 'test')
+
+for s in ${servers[@]}; do
+    mv ${logs_path}${s}/access.log ${logs_path}${s}/access-$(date -d "yesterday" +%Y-%m-%d).log
+    mv ${logs_path}${s}/error.log ${logs_path}${s}/error-$(date -d "yesterday" +%Y-%m-%d).log
+done
+
+#mv ${logs_path}error.log ${logs_path}error-$(date -d "yesterday" +%Y-%m-%d).log
+
+kill -USR1 `cat ${pid_path}`
