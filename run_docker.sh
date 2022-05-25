b0a559670e67da7050ab9ab032399548c95ccee0
IfanTsai <i@caiyifan.cn>
2022-05-25 23:19:33 +0800

    update run_docekr script

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,15 @@
 
 docker build -t my_blog .
 
-docker run -itd -v /etc/letsencrypt:/etc/letsencrypt -v json:/usr/local/openresty/nginx/conf/json -v /etc/nginx/.htpasswd:/etc/nginx/.htpasswd -v /usr/sbin/modprobe:/usr/sbin/modprobe -v /lib/modules:/lib/modules -v /usr/src:/usr/src --net=host --rm --privileged my_blog
+docker volume create openresty
+
+docker run -itd -v \
+/etc/letsencrypt:/etc/letsencrypt \
+-v /etc/nginx/.htpasswd:/etc/nginx/.htpasswd \
+-v /usr/sbin/modprobe:/usr/sbin/modprobe \
+-v /lib/modules:/lib/modules \
+-v /usr/src:/usr/src \
+-v /var/www/hexo:/var/www/hexo \
+-v /usr/local/blog/json:/usr/local/openresty/nginx/conf/json \
+-v openresty:/usr/local/openresty \
+--net=host --rm --privileged my_blog
